a7f3d13e2cbec91b5d9629da962a9a96d503c747
wimarko <marko.wiik@metropolia.fi>
2024-04-02 08:37:42 +0300

    teht 6 gameworld async

--- Tehtava_3_Rinnakkaista_Pelin_Laskentaa/GameWorld.cpp
+++ Tehtava_3_Rinnakkaista_Pelin_Laskentaa/GameWorld.cpp
@@ -7,82 +7,55 @@
 #include <memory>
 #include <mutex>
 #include <chrono>
+#include <future>
 
 using namespace std::chrono;
 
 
 class Semaphore {
-public: Semaphore (int allowed_threads) : thread_count_(allowed_threads) {}
 
-	  void acquire() 
-	  {
-		  //unique lock: when going to wait, release mutex-lock..or smthing
-		  std::unique_lock<std::mutex> lock(mute_);
-		  while (thread_count_ == 0) {
-			  condition_.wait(lock);  
-		  }
-		  thread_count_--;
-	  }
-
-	  void release() 
-	  {
-		  std::lock_guard<std::mutex> lock(mute_);
-		  thread_count_++;
-		  condition_.notify_one();
-	  }
 
 private:
 	std::mutex mute_;
 	std::condition_variable condition_;
-	int thread_count_;
 };
 
 
 int main() {
 
-	std::vector<std::unique_ptr<Game_Task>> tasks;
-	std::vector<std::thread> threadsVector;
-
-	unsigned int numThreads = std::thread::hardware_concurrency();
-	unsigned int maxThreads = numThreads - 1;
-	unsigned int usedThreads = 0;
-	Semaphore threadSemaphore(maxThreads);
-
-	std::cout << "Using threads in machine: " << maxThreads << "\n";
+	std::vector<std::unique_ptr<Class_A>> tasks;
+	std::vector<std::future<void>> futuresVector;
 
 	int timeDelay = 3;
 	int amount_of_tasks = 30;
+
+	
+
+	int number_of_tasks = tasks.size();
 
 	//create "Tasks"-Class_As, give each an id  int(i)
 	for (int i = 0; i < amount_of_tasks; i++) {
 		tasks.emplace_back(std::make_unique<Class_A>(i));
 	}
 
-
-	int number_of_tasks = tasks.size();
 	//auto ~ std::chrono.... k::time_point makes it shorter
 	auto start = high_resolution_clock::now();
 
 	//Add each task to threads-vector
 	for (auto& task : tasks)
 	{
-		threadSemaphore.acquire();
-		threadsVector.emplace_back([&task, timeDelay,
-		&threadSemaphore, &usedThreads]() 
-			{
+		futuresVector.emplace_back(std::async(std::launch::async, [&task, timeDelay]() {
 			task->Perform(timeDelay);
-			threadSemaphore.release();
-			});
-		usedThreads++;
+			}));
 	}
 
-	for (auto& thread : threadsVector)
+	for (auto& future : futuresVector)
 	{
-		thread.join();
+		future.get();
 	}
 	//continue only after all tasks are complete!
 
-	std::cout << "Program finished.\n";
+	std::cout << "Program  async gameworld finished.\n";
 	auto stop = high_resolution_clock::now();
 
 	auto duration =
